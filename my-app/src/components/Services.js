a70941c303f3702116e84bbf855bd8c5362808f3
JoshuaAldrich <joshuataldrich@gmail.com>
2022-11-22 16:42:53 -0500

    Changes to services

--- my-app/src/components/Services.js
+++ my-app/src/components/Services.js
@@ -1,3 +1,21 @@
-const Services = () => {};
+const Services = () => {
+  return (
+    <>
+      <div>
+        <h2>Our Services</h2>
+        <p>
+          We offer a wide range of services to commercial contractors throughout
+          Florida.
+        </p>
+        <p>
+          Our team strives to provide quick delivery and installation of our
+          products. All of the field installers are well versed in today's ADA
+          standards and requirements.
+        </p>
+        <div></div>
+      </div>
+    </>
+  );
+};
 
 export default Services;
